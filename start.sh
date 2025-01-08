cd0783a102058f05a26e25a33eb291ae6346871a
SinhoGamer <90977235+SinhoGamer@users.noreply.github.com>
2025-01-07 22:44:02 -0300

    melhorias

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 GREEN='\033[0;32m'
 while : 
 do
-echo -e "${GREEN} Aleatory - Auto reconexão ativada para prevenção de quedas.."
+echo -e "${GREEN} Ligando BOT..."
 
 if [ "$1" = "sim" ]; then
 node iniciar.js sim
